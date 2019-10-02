f551db026f12d5d9db65765ff970ed7c0792a693
Danilo Zeni <danilomde@gmail.com>
2019-10-02 15:06:09 -0300

    Paralax novo

--- wp-content/themes/jumperfranquia/footer.php
+++ wp-content/themes/jumperfranquia/footer.php
@@ -48,12 +48,13 @@
 
 <!-- Bootstrap -->
 <script src="<?php bloginfo( 'template_url' ); ?>/assets/bootstrap-4.3.1/js/bootstrap.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-<!-- simpleParallax -->
-<script src="<?php bloginfo( 'template_url' ); ?>/assets/simpleParallax/dist/simpleParallax.min.js"></script>
+
+
+<!-- arallax -->
+
+<script src="<?php bloginfo( 'template_url' ); ?>/assets/parallax.js-1.5.0/parallax.min.js"></script>
+
 
 
 <!-- scripts -->
