ac5974391b377cfa311e3e42352c950449f98fcb
Danilo Zeni <danilomde@gmail.com>
2019-10-03 17:33:02 -0300

    Scrollto

--- wp-content/themes/jumperfranquia/footer.php
+++ wp-content/themes/jumperfranquia/footer.php
@@ -14,7 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="footer" class="site-footer">
-		<div class="row boxfooter">
+		<div class="row boxfooter" id="target">
 			<div class="col-md-6 callfooter ">
 				<h2><strong>Multiplique</strong> o seu <strong class="bigstrong">dinheiro</strong></h2>
 
@@ -41,7 +41,7 @@
 </div><!-- #page -->
 
 
-<a href="https://api.whatsapp.com/send?phone=5541999120265&text=Ol%C3%A1,%20tenho%20interesse%20em%20comprar%20uma%20franquia%20JUMPER!" class="btnwhats">
+<a href="https://api.whatsapp.com/send?phone=5541999120265&text=Ol%C3%A1,%20tenho%20interesse%20em%20comprar%20uma%20franquia%20JUMPER!" class="btnwhats" target="_blank">
 	<img src="<?php bloginfo( 'template_url' ); ?>/images/whats.png">
 </a>
 
@@ -51,16 +51,15 @@
 <!-- jQuery -->
 <script src="<?php bloginfo( 'template_url' ); ?>/assets/jQuery-3.4.1/jquery-3.4.1.min.js"></script>
 
+
 <!-- Bootstrap -->
 <script src="<?php bloginfo( 'template_url' ); ?>/assets/bootstrap-4.3.1/js/bootstrap.min.js"></script>
 
-
-
-<!-- arallax -->
-
+<!-- Parallax -->
 <script src="<?php bloginfo( 'template_url' ); ?>/assets/parallax.js-1.5.0/parallax.min.js"></script>
 
-
+<!-- ScrollTo -->
+<script src="<?php bloginfo( 'template_url' ); ?>/assets/animatescroll.js-master/animatescroll.min.js"></script>
 
 <!-- scripts -->
 <script src="<?php bloginfo( 'template_url' ); ?>/js/scripts.js"></script>
