839c865c32311434e57a1b36858b6b370103a40c
Danilo Zeni <danilomde@gmail.com>
2019-10-15 10:12:57 -0300

    plugin

---
+++ wp-content/themes/jumperfranquia/page-home-mobile.php
@@ -0,0 +1,520 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package JUMPER_Franquias
+ */
+
+get_header();
+?>
+	<!-- SECTION 2 e 3 -->
+	<section class="section2">
+		<div class="container">
+			<div class="row rowSection2Bg">
+				
+				<div class="col-md-6 section2AjusteTopo">
+					<h2>Como está a situação dos <strong>jovens</strong> no <strong>mercado de trabalho?</strong></h2>
+				</div>
+
+				<div class="col-md-6 section2AjusteTopo">
+					<h3><span class="bignumber">93%</span> dos jovens  entre 13 e 18 anos, não sabem o que irão fazer no futuro profissional.</h3>
+				</div>
+
+				<div class="col-md-12 text-center meninoCarteira"> 
+					<img src="<?php bloginfo( 'template_url' ); ?>/images/p2/pessoa-p2.png">	
+				</div>
+
+				<div class="col-md-6" id="textoMenino1">
+					<div class="texto1Section2">
+						<h2>Os <strong>jovens sofrem</strong> para conseguir o <strong>primeiro emprego</strong>, é a famosa geração <strong>"NEM, NEM, NEM"</strong> nem estuda, nem trabalha, nem procura emprego.</h2>
+					</div>
+					
+				</div>
+
+				<div class="col-md-6 text-center" id="textoMenino2">
+					<div class="texto2Section2">
+						<h3><strong>Sabe por que isso acontece?</strong></h3> <p>Estão completamente desqualificados para trabalhar, e por isso, não conseguem emprego</p>
+					</div>
+					
+				</div>
+
+
+				<div class="col-md-12 text-center" id="textMenino3">
+					<h1>Mas, se fizerem 1 curso, vão mudar essa situação?</h1>
+					<h3 style="background-color: yellow;">NÃO</h3>
+					<h4>Somente com 1 curso o jovem ainda não se qualifica o suficiente. Para trabalhar ele precisa: </h4>
+				</div>
+				
+				<div class="row" id="rowAjusteSec2">
+					<div class="col-md-3 text-center sec2DivNoMax">
+						<img width="100px" height="100px" style="width: 26%;" src="<?php bloginfo( 'template_url' ); ?>/images/p2/icon-pt2-1.png" >
+						<p>Ser empreendedor e diferenciado</p>
+					</div>
+					<div class="col-md-3 text-center sec2DivNoMax" style="padding-top: 1%;">
+						<img  width="100px" height="100px" src="<?php bloginfo( 'template_url' ); ?>/images/p2/icon-pt2-2.png">
+						<p>Falar outro idioma</p>
+					</div>
+					<div class="col-md-3 text-center sec2DivNoMax">
+						<img  width="100px" height="100px" src="<?php bloginfo( 'template_url' ); ?>/images/p2/icon-pt2-3.png">
+						<p>Dominar a tecnologia ultilizada pelas empresas</p>
+					</div>
+					<div class="col-md-3 text-center sec2DivNoMax">
+						<img  width="100px" height="100px" src="<?php bloginfo( 'template_url' ); ?>/images/p2/icon-pt2-4.png">
+						<p>Conhecer sobre as rotinas administrativas</p>
+					</div>
+
+
+					<div class="col-md-3 text-center sec2DivNoMax">
+						<img  width="100px" height="100px" src="<?php bloginfo( 'template_url' ); ?>/images/p2/icon-pt2-5.png">
+						<p>Atender bem o cliente</p>
+					</div>
+					<div class="col-md-3 text-center sec2DivNoMax">
+						<img  width="100px" height="100px" src="<?php bloginfo( 'template_url' ); ?>/images/p2/icon-pt2-6.png">
+						<p>Saber negociar e vender</p>
+					</div>
+
+					<div class="col-md-3 text-center">
+						
+						<div class="row">
+							<div class="col-md-6">
+								<img width="100px" height="100px"  src="<?php bloginfo( 'template_url' ); ?>/images/p2/icon-pt2-7.png">
+								<p>Saber trabalhar em equipe</p>
+							</div>
+
+							<div class="col-md-6">
+								<img  width="100px" height="100px" src="<?php bloginfo( 'template_url' ); ?>/images/p2/icon-pt2-8.png">
+								<p>Falar em público</p>
+							</div>
+						</div>
+						
+
+					</div>
+
+
+					<div class="col-md-3 text-center sec2DivNoMax">
+						<img  width="100px" height="100px" src="<?php bloginfo( 'template_url' ); ?>/images/p2/icon-pt2-9.png">
+						<p>Estar preparado para uma entrevista de emprego</p>
+					</div>
+
+
+				</div>
+				
+				<div class="row text-center" id="section2RowAjuste4">
+
+					<div class="ajusteH2RowAjuste4">
+						<h2>Quando o jovem possui todas estas <strong>qualificações,</strong> se destaca em um processo seletivo, e consegue o seu <strong>primeiro emprego.</strong></h2>
+					</div>
+					
+
+					<div class="ajusteLinha1RowAjuste4">
+						<img src="<?php bloginfo( 'template_url' ); ?>/images/p2/flecha1.png">
+					</div>
+					
+				</div>
+
+				<div class="row">
+					<div class="ajusteSomenteUmaFormacao">
+						<h2>"Somente uma <strong>formação completa</strong> muda a vida das pessoas"</h2>
+
+						<div class="text-right">
+							<p>- Everton Sabú</p>
+						</div>
+						
+						<p>O salário desse primeiro emprego, transforma não somente a vida desse jovem, mas de toda familia.</p>
+
+						<div class="ajusteLinha2RowAjuste4">
+							<img src="<?php bloginfo( 'template_url' ); ?>/images/p2/flecha2.png">
+						</div>
+
+						<div class="ajusteClasseCeD">	
+							<h2>E as classes <strong>C e D,</strong> conseguem pagar todos esses cursos?</h2>
+							<p>Na concorrência, não. Pois, os cursos necessários, são cobrados separadamente.</p>
+
+							<p>Mas se vier na <strong>JUMPER!</strong>, terá <strong>Formação completa,</strong> irá fazer cursos, com valor único e acessível, nas áreas de: </p>
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="ajusteLinha3RowAjuste4">
+					<img src="<?php bloginfo( 'template_url' ); ?>/images/p2/flecha3.png">
+				</div>
+
+			<div class="row rowSection3Bg">
+				<div class="col-md-12 imgEscolaSec3">
+					<img src="<?php bloginfo( 'template_url' ); ?>/images/p3/escola-sec3.png">
+				</div>
+
+				<div class="container">
+					<div class="row rowContainerSec3 ">
+						<div class="col-md-2 text-left">
+							<img src="<?php bloginfo( 'template_url' ); ?>/images/p3/icon-sec3-1.png" width="125px" height="125px">
+							<img class="iconMais" src="<?php bloginfo( 'template_url' ); ?>/images/p3/mais.png" width="50px" height="50px">
+							
+						</div>
+
+						<div class="col-md-2 text-center">
+							<img src="<?php bloginfo( 'template_url' ); ?>/images/p3/icon-sec3-2.png" width="125px" height="125px">
+							<img class="iconMais" src="<?php bloginfo( 'template_url' ); ?>/images/p3/mais.png" width="50px" height="50px">
+						</div>
+
+						<div class="col-md-2 text-center">
+							<img src="<?php bloginfo( 'template_url' ); ?>/images/p3/icon-sec3-3.png" width="125px" height="125px">
+							<img class="iconMais" src="<?php bloginfo( 'template_url' ); ?>/images/p3/mais.png" width="50px" height="50px">
+						</div>
+
+						<div class="col-md-2 text-center">
+							<img src="<?php bloginfo( 'template_url' ); ?>/images/p3/icon-sec3-4.png" width="125px" height="125px">
+							<img class="iconMais" src="<?php bloginfo( 'template_url' ); ?>/images/p3/mais.png" width="50px" height="50px">
+						</div>
+
+						<div class="col-md-2 text-center" id="ultimoIcon">
+							<img src="<?php bloginfo( 'template_url' ); ?>/images/p3/icon-sec3-5.png" width="125px" height="125px">
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12" id="rowBgTreinamentoSection3">
+					<!-- <div class="container"> -->
+						<div class="row justify-content-md-center" id="ajusteRowTreinamentoSection3">
+							<div class="col-md-3 text-right divBorderRightSec3">
+								<p>Além disso, durante a sua formação receberá</p>
+							</div>
+
+							<div class="col-md-3 text-left">
+								<h2 id="h2AjusteSec3">12 treinamentos <span>complementares gratuitos</span></h2>
+							</div>
+						</div>
+					<!-- </div> -->
+				</div>
+
+				<div class="col-md-6" id="bgMeninaApontandoSec3">
+				</div>
+
+				<div class="col-md-6" id="rowListaMeninaApontandoSec3">
+					<ul>
+						<li>1. Liderança</li>
+						<li>2. Técnicas de Entrevistas de Emprego</li>
+						<li>3. Comnunicação</li>
+						<li>4. Expressão Corporal</li>
+						<li>5. Marketing Pessoal</li>
+						<li>6. Trabalho em Equipe</li>
+						<li>7. Solução de Conflitos no Trabalho</li>
+						<li>8. Finanças Pessoais</li>
+						<li>9. Atendimento ao Cliente</li>
+						<li>10. Auxiliar de Escritório</li>
+						<li>11. Telemarketing</li>
+						<li>12. Elaboração de Currículo</li>
+					</ul>
+				</div>
+				
+
+			</div>
+		</div>
+	</section>
+
+	<!-- SECTION 4 -->
+	<section class="section4">
+		<div class="container">
+			<div class="row" id="ajusteRow1Sec4">
+				<div class="col-md-10 text-left" id="div1Sec4">
+					<h1 id="h1Div1Sec4">Mas,</h1>
+					<h1 id="h2Div1Sec4">e o preço</h1>
+				</div>
+				<div class="col-md-2" id="div2Sec4">
+					<h1 id="h1Div2Sec4"> ? </h1>
+				</div>
+			</div>
+
+			<div class="row justify-content-md-center">
+
+				<div class="col-md-4" id="div3Sec4" >
+
+					<p><strong>Toda essa formação</strong> custaria:</p>
+					<div>
+						<img src="<?php bloginfo( 'template_url' ); ?>/images/p4/x.png">
+						<h3>R$<span>697</span>,00 / mês</h3>
+					</div>
+					
+				</div>
+				<div class="col-md-4" id="div4Sec4" >
+					<p>Sabemos que esse valor, não cabe no orçamento das famílias das <strong>classes C e D.</strong></p>
+				</div>
+			</div>
+
+			<div class="row justify-content-md-center">
+				<div class="col-md-4" id="div5Sec4">
+					<p>Na <strong>JUMPER!</strong>, estamos comprometidos em transformar a vida das pessoas. Por isso, criamos o programa <strong>FORMATEC,</strong> que oferece condições acessíveis a todos.</p>
+				</div>
+
+				<div class="col-md-4" id="div6Sec4">
+					<p>O aluno faz todos os cursos <strong>(Formação Completa)</strong> por: </p>
+					<h3>R$<span>249</span>,00 / mês</h3>
+				</div>
+			</div>
+		</div>
+  	</section>
+
+  	<!-- SECTION 5 -->
+  	<section class="section5">
+  		<span style="display: none;">já transformamos a vida de mais de 600 mil pessoas</span>
+  	</section>
+
+  	<!-- SECTION 6 -->	
+  	<section class="section6">
+  		<div class="container">
+
+  			<div class="row justify-content-md-left" id="ajusteRowSec6">
+  				<div class="col-md-4" id="div1Sec6">
+  					<p>Sabe como o aluno</p>
+  					<h2>estuda na Jumper</h2>
+  				</div>
+  				<div class="col-md-1" id="div2Sec6">
+  					<h1>?</h1>
+  				</div>
+  			</div>
+
+  			<div class="row justify-content-md-left" id="ajusteRow2Sec6">
+  				<div class="col-md-2 text-right" id="div3Sec6">
+  					<p>As aulas são:</p>
+  				</div>
+  				<div class="col-md-3">
+  					<h3><strong>Presencias, Práticas, Em grupo e Divertidas</strong></h3>
+  				</div>
+  			</div>
+
+  			<div class="row justify-content-md-left" id="ajusteRow3Sec6">
+  				<div class="col-md-2 text-right" id="div4Sec6">
+  					<p>O aluno aprende de</p>
+  					<h3><strong>verdade!</strong></h3>
+  				</div>
+
+  				<div class="col-md-4" id="div5Sec6">
+  					<p>Existem milhares de jovens na sua cidade esperando esta oportunidade de estudar na <strong>JUMPER!</strong></p>
+  				</div>
+  			</div>
+
+  			<div class="row">
+  				<div class="col-md-6 text-center" id="div6Sec6">
+  					<div class="subDiv6">
+  						<h3>"</h3>
+  					</div>
+
+  					<div class="subDiv6">
+  						<p>Você pode ser um <strong>parceiro nesta Missão!</strong></p>
+  					</div>
+  				</div>
+  			</div>
+
+  		</div>
+  	</section>
+	
+	<!-- SECTION 7 -->
+  	<section class="section7">
+  		<div class="container">
+  			<div class="row justify-content-md-center">
+  				<div class="col-md-12 text-center" id="ajusteDivSec7">
+  					<h2 style="display: none;">Conheça a sua unidade</h2>
+  					<h1 style="display: none;">JUMPER<strong>!</strong></h1>
+  					<img src="<?php bloginfo( 'template_url' ); ?>/images/p7/conheca-a-jumper.png">
+  				</div>
+  			</div>
+  		</div>
+  	</section>
+
+  	<section class="section8">
+  			<div id="row1Sec8Bg">
+  				
+  			</div>
+  				<div class="row section8Imgs">
+
+  					<div class="col-md-2">
+  						<img src="<?php bloginfo( 'template_url' ); ?>/images/p8/recepcao.jpg">
+  					</div>
+
+  					<div class="col-md-2">
+  						<img src="<?php bloginfo( 'template_url' ); ?>/images/p8/idiomas.jpg">
+  					</div>
+
+  					<div class="col-md-2">
+  						<img src="<?php bloginfo( 'template_url' ); ?>/images/p8/industrial.jpg">
+  					</div>
+
+  					<div class="col-md-2">
+  						<img src="<?php bloginfo( 'template_url' ); ?>/images/p8/informatica.jpg">
+  					</div>
+
+  					<div class="col-md-2">
+  						<img src="<?php bloginfo( 'template_url' ); ?>/images/p8/beleza.jpg">
+  					</div>
+
+  					<div class="col-md-2">
+  						<img src="<?php bloginfo( 'template_url' ); ?>/images/p8/mecanica.jpg">
+  					</div>
+
+  				</div>
+  	</section>
+
+
+  	<section class="section9">
+  		<div class="container">
+
+  			<div class="row">
+  				<div class="col-md-12" id="ajusteDiv1Sec9">
+  					<h2>Estaremos com </h2>
+  					<h1>Você</h1>
+  				</div>
+  			</div>
+
+  			<div class="row mt-n4">
+  				
+  				<div class="col pl-4 pr-5 tabuleiroAjuste offset-md-4" >
+  					<div class="row">
+  						<div class="col-md-2 h1Sec9">
+  							<h1>1</h1>
+  						</div>
+
+  						<div class="col pSec9">
+  							<p>Auxílio na escolha do imóvel</p>
+  						</div>
+  					</div> 
+  					
+  				</div>
+
+  				<div class="col pl-4 pr-5 tabuleiroAjuste" >
+  					<div class="row">
+  						<div class="col-md-2 h1Sec9">
+  							<h1>2</h1>
+  						</div>
+
+  						<div class="col pSec9">
+  							<p>Orientação na Montagem da Estrutura</p>
+  						</div>
+  					</div> 
+  				</div>
+
+  				<div class="col pl-4 pr-5 tabuleiroAjuste" >
+					<div class="row">
+						<div class="col-md-2 h1Sec9">
+							<h1>3</h1>
+						</div>
+
+						<div class="col pSec9">
+							<p>Projeto Arquitetônico</p>
+						</div>
+					</div> 
+  				</div>
+  			</div>
+
+  			<div class="row" id="rowAjusteSec9">
+  				<div class="col offset-md-3 tabuleiroAjuste2" styl>
+  					<div class="row ml-5">
+
+  						<div class="col-md-2 h1Sec92">
+  							<h1>6</h1>
+  						</div>
+
+  						<div class="col pSec92 ml-3 mt-2">
+  							<p>Kit Material didático</p>
+  						</div>
+
+  					</div>
+  				</div>
+
+  				<div class="col tabuleiroAjuste2">
+  					<div class="row mt-2">
+
+  						<div class="col-md-2 h1Sec92">
+  							<h1>5</h1>
+  						</div>
+
+  						<div class="col pSec92">
+  							<p>Ações de Marketing para captação de alunos</p>
+  						</div>
+
+  					</div>
+  				</div>
+
+  				<div class="col tabuleiroAjuste2">
+  					<div class="row mt-2">
+
+  						<div class="col-md-2 h1Sec92">
+  							<h1>4</h1>
+  						</div>
+
+  						<div class="col pSec92">
+  							<p>Assistência na contratação da equipe</p>
+  						</div>
+
+  					</div>
+  				</div>
+  			</div>
+
+  			<div class="row justify-content-md-center" id="row3AjusteSec9">
+
+  				<div class="col-md-3 pr-5" >
+  					<div class="row">
+
+  						<div class="col-md-2 h1Sec93 mr-2">
+  							<h1>7</h1>
+  						</div>
+
+  						<div class="col pSec93 ml-n3">
+  							<p>Evento de Inauguração</p>
+  						</div>
+  						
+  					</div>
+  				</div>
+
+  				<div class="col-md-2 ml-n2" >
+  					<div class="row">
+
+  						<div class="col-md-2 h1Sec93 ml-n4 mr-4">
+  							<h1>8</h1>
+  						</div>
+
+  						<div class="col pSec93">
+  							<p>Suporte total da franqueadora</p>
+  						</div>
+  						
+  					</div>
+  				</div>
+
+  				<div class="col-md-3 pr-5 pl-5" >
+  					<div class="row">
+
+  						<div class="col-md-2 h1Sec93">
+  							<h1>9</h1>
+  						</div>
+
+  						<div class="col pSec93">
+  							<p>30 dias de consultoria na unidade</p>
+  						</div>
+  						
+  					</div>
+  				</div>
+
+  			</div>
+
+  		</div>
+  	</section>
+
+
+	<section class="namidia">
+		<div class='titlenamidia'>
+			<img src="<?php bloginfo( 'template_url' ); ?>/images/jumper-na-midia.png" class="mt-5 ml-5" alt="JIMPER NA MÍDIA" title="JIMPER NA MÍDIA">
+		</div>
+	</section>
+	
+	
+<?php
+
+get_footer();
